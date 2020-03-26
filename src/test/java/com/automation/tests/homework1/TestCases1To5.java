fb1dc19c25ac994dbc27a6db5c549f8ee45ed336
AndreaKrizbai <krizbai.andrea@gmail.com>
2020-03-26 17:57:01 -0400

    test 2 through 4

--- src/test/java/com/automation/tests/homework1/TestCases1To5.java
+++ src/test/java/com/automation/tests/homework1/TestCases1To5.java
@@ -12,6 +12,10 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TestCases1To5 {
     private RemoteWebDriver driver;
 
@@ -26,11 +30,32 @@ public class TestCases1To5 {
 
     @Test
     public void test2(){
-
+        List<WebElement>progLangs = driver.findElements(By.xpath("//label[@class='form-check-label']"));
+        List<String> expected = new ArrayList<>();
+        expected.add("C++");
+        expected.add("Java");
+        expected.add("JavaScript");
+        Assert.assertEquals(BrowserUtils.getTextFromWebElements(progLangs), expected);
     }
 
+    @Test
+    public void test3(){
+        WebElement firstName = driver.findElement(By.name("firstname"));
+        firstName.sendKeys("a");
+        String expected = "first name must be more than 2 and less than 64 characters long";
+        WebElement actual = driver.findElement(By.xpath("//small[@data-bv-validator='stringLength']"));
+        Assert.assertEquals(actual.getText(), expected);
+    }
 
-
+    @Test
+    public void test4(){
+        WebElement lastName = driver.findElement(By.name("lastname"));
+        lastName.sendKeys("a");
+        //String expected = "The last name must be more than 2 and less than 64 characters long";
+        WebElement actual = driver.findElement(By.xpath("//small[@data-bv-for='lastname'][2]"));
+       //Assert.assertEquals(actual.getText(), expected);
+        Assert.assertTrue(actual.isDisplayed());
+    }
 
     @BeforeMethod
     public void setup(){
